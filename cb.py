ab41cd3b7c2d205f3a395592d19ee96634a28522
rawi142 <rawia.ben.amira1997@gmail.com>
2023-08-31 00:38:06 +0200

    edit

--- cb.py
+++ cb.py
@@ -3,14 +3,12 @@
 un simple chatBot d'Assistances aux inscriptions universitaires
 """
 import nltk
-from nltk.chat.util import Chat, reflections
-
+from nltk.chat import Chat, reflections
 nltk.download("punkt")
 nltk.download("averaged_perceptron_tagger")
 nltk.download("wordnet")
-
 question_reponse = [
-        [ 
+        [
             r"(.*)date limite(.*)",
            [ "date limite d inscription est le 30/09"]
         ],
@@ -20,21 +18,23 @@ question_reponse = [
         ],
         [
             "r(.*)documents(.*)",
-            ["les documents necessaires sont une copie de votre piece d'identité, relevé de note de l'année precedente et notification bourse si vous etes boursier"],
+            ["les documents necessaires sont une copie de",
+            "votre piece d'identité, relevé de note de l'année precedente ",
+            "et notification bourse si vous etes boursier"],
         ]
         ]
-def chat_bot(question_reponse):
+def chat_bot():
     """
-    fonction qui definit le chatbot
+    fonction qui fais appel aux paires de qestion reponses
     """
     chatbot = Chat(question_reponse, reflections)
-    print("Bonjour ! Comment puis-je vous aider avec les inscriptions ?")
+    print("Bonjour ! Comment puis-je vous aider avec votre inscription ?")
     while True:
-        user_input = input("> ")
-        if user_input.lower() == "exit":
+        entree = input(">>> ")
+        if entree.lower() == "quitter":
             print("Au revoir !")
             break
-        response = chatbot.respond(user_input)
-        print(response)
+        reponse = chatbot.respond(entree)
+        print(reponse)
 if __name__ == "__main__":
-    chat_bot(question_reponse)
+    chat_bot()
