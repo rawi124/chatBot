5634695b6a7d3ed7ace5bd2278bc62dd183e0067
rawi142 <rawia.ben.amira1997@gmail.com>
2023-08-30 17:09:35 +0200

    ajout des ressources linguistiques necessaires a NLTK

--- cb.py
+++ cb.py
@@ -1,2 +1,8 @@
 import nltk
-from nltk.chat.util import Chat, reflecions
+from nltk.chat.util import Chat, reflections
+
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
+nltk.download("wordnet")
+
+
