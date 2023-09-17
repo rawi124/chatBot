ba825b529545df20c547a652ecf2f961f7aff4e0
rawi142 <rawia.ben.amira1997@gmail.com>
2023-09-17 14:00:20 +0200

    ajout exception

--- cb.py
+++ cb.py
@@ -7,22 +7,34 @@ from nltk.chat.util import Chat, reflections
 nltk.download("punkt")
 nltk.download("averaged_perceptron_tagger")
 nltk.download("wordnet")
+
+
+class ExpressionInvalide(Exception):
+    """
+    exception si la question posée ne 
+    match avec aucune question du chatBot
+    """
+    pass
+
+
 question_reponse = [
-        [
-            r"(.*)date limite(.*)",
-           [ "date limite d inscription est le 30/09"]
-        ],
-        [
-            r"(.*)frais(.*)",
-            ["les frais d'inscription pour la formation sont de 190 euros"]
-        ],
-        [
-            r"(.*)documents(.*)",
-            ["les documents necessaires sont une copie de"
-            +"votre piece d'identité, relevé de note de l'année precedente "
-           + "et notification bourse si vous etes boursier"],
-        ]
-        ]
+    [
+        r"(.*)date limite(.*)",
+        ["date limite d inscription est le 30/09"]
+    ],
+    [
+        r"(.*)frais(.*)",
+        ["les frais d'inscription pour la formation sont de 190 euros"]
+    ],
+    [
+        r"(.*)documents(.*)",
+        ["les documents necessaires sont une copie de"
+         + "votre piece d'identité, relevé de note de l'année precedente "
+         + "et notification bourse si vous etes boursier"],
+    ]
+]
+
+
 def chat_bot():
     """
     fonction qui fais appel aux paires de qestion reponses
@@ -30,11 +42,22 @@ def chat_bot():
     chatbot = Chat(question_reponse, reflections)
     print("Bonjour ! Comment puis-je vous aider avec votre inscription ?")
     while True:
-        entree = input(">>> ")
-        if entree.lower() == "quitter":
-            print("Au revoir !")
-            break
-        reponse = chatbot.respond(entree)
-        print(reponse)
+        try:
+            entree = input(">>> ")
+            if entree.lower() == "quitter":
+                print("Au revoir !")
+                break
+            if not entree:
+                raise ExpressionInvalide(
+                    "saisie non valide, veuillez entrer une autre expression")
+            reponse = chatbot.respond(entree)
+            if reponse is None:
+                raise ExpressionInvalide(
+                    "désolé je ne comprend pas votre demande")
+            print(reponse)
+        except Exception as e:
+            print(f"une erreur c'est produite : {e}")
+
+
 if __name__ == "__main__":
     chat_bot()
